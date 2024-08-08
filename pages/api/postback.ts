f78565e11261013db07ce5e5639039ae3650234e
Michal <m@michals-mbp.home>
2024-08-08 07:22:10 +0200

    added postbacks13

--- pages/api/postback.ts
+++ pages/api/postback.ts
@@ -4,7 +4,7 @@ const prisma = new PrismaClient();
 
 export default async function handler(req: any, res: any) {
   if (req.method === "GET") {
-    const { id: offer_id, offer_name, affiliate_id, source, session_ip, payout, datetime, session_timestamp, aff_sub, aff_sub2, aff_sub3, aff_sub4, aff_sub5 } = req.query;
+    const { id: offer_id, offer_name, affiliate_id, source, session_ip, payout, datetime, aff_sub, aff_sub2, aff_sub3, aff_sub4, aff_sub5 } = req.query;
 
     // Simple validation
     if (!offer_id) {
@@ -15,6 +15,17 @@ export default async function handler(req: any, res: any) {
       const postback = await prisma.postback.create({
         data: {
           offer_id,
+          offer_name,
+          affiliate_id,
+          source,
+          session_ip,
+          payout: parseFloat(payout),
+          datetime,
+          aff_sub,
+          aff_sub2,
+          aff_sub3,
+          aff_sub4,
+          aff_sub5,
         },
       });
       res.status(200).json(postback);
