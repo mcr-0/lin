44edb156c2a16c6848852f269406d7d688e7e376
Michal <m@michals-mbp.home>
2024-08-12 11:47:33 +0200

    postback new

--- app/api/postback/route.ts
+++ app/api/postback/route.ts
@@ -26,24 +26,56 @@ export async function GET(req: NextRequest) {
   }
 
   try {
-    const postback = await prisma.conversions.create({
+    const conversions = await prisma.conversions.create({
       data: {
-        offer_id: offer_id as any,
+        offer_id: offer_id as string | undefined,
         offer_name: offer_name as string | undefined,
         affiliate_id: affiliate_id as string | undefined,
         source: source as string | undefined,
         session_ip: session_ip as string | undefined,
         payout: payout ? parseFloat(payout as string) : undefined,
-
         aff_sub: aff_sub as string | undefined,
         aff_sub2: aff_sub2 as string | undefined,
         aff_sub3: aff_sub3 as string | undefined,
         aff_sub4: aff_sub4 as string | undefined,
         aff_sub5: aff_sub5 as string | undefined,
-        userid: userid as string | undefined,
       },
     });
-    return NextResponse.json(postback);
+
+    // 2. Sprawdzenie, czy istnieje rekord w Activity z odpowiednim userId i offer_id
+    const existingActivity = await prisma.activity.findFirst({
+      where: {
+        userId: aff_sub5 ?? "",
+        offer_id: offer_id ?? "", // Oczekiwany typ string
+      },
+    });
+
+    if (existingActivity) {
+      // 3. Jeśli istnieje, zaktualizuj rekord dodając nową wartość payout
+      await prisma.activity.update({
+        where: {
+          id: existingActivity.id,
+        },
+        data: {
+          payout: {
+            increment: conversions.payout ?? 0,
+          },
+        },
+      });
+    } else {
+      // 4. Jeśli rekord nie istnieje, utwórz nowy
+      await prisma.activity.create({
+        data: {
+          action: "conversion",
+          offer_id: offer_id ?? "", // Oczekiwany typ string
+          converted: 1,
+          timestamp: new Date(),
+          userId: aff_sub5 ?? "",
+          payout: conversions.payout ?? 0,
+        },
+      });
+    }
+    return NextResponse.json(conversions);
   } catch (error) {
     console.error(error);
     return NextResponse.json({ error: "Failed to save postback" }, { status: 500 });
---
+++ prisma/migrations/20240809150724_init/migration.sql
@@ -0,0 +1,99 @@
+-- CreateTable
+CREATE TABLE "conversions" (
+    "id" SERIAL NOT NULL,
+    "createdat" TIMESTAMPTZ(0) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "offer_id" TEXT,
+    "offer_name" TEXT,
+    "affiliate_id" TEXT,
+    "source" TEXT,
+    "session_ip" TEXT,
+    "payout" DOUBLE PRECISION,
+    "aff_sub" TEXT,
+    "aff_sub2" TEXT,
+    "aff_sub3" TEXT,
+    "aff_sub4" TEXT,
+    "aff_sub5" TEXT,
+    "userid" TEXT,
+
+    CONSTRAINT "conversions_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "username" TEXT NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Activity" (
+    "id" TEXT NOT NULL,
+    "action" TEXT,
+    "offer_id" INTEGER,
+    "payout" DOUBLE PRECISION,
+    "converted" INTEGER,
+    "timestamp" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "userId" TEXT NOT NULL,
+
+    CONSTRAINT "Activity_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Account" (
+    "id" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "type" TEXT NOT NULL,
+    "provider" TEXT NOT NULL,
+    "providerAccountId" TEXT NOT NULL,
+    "refresh_token" TEXT,
+    "access_token" TEXT,
+    "expires_at" INTEGER,
+    "token_type" TEXT,
+    "scope" TEXT,
+    "id_token" TEXT,
+    "session_state" TEXT,
+
+    CONSTRAINT "Account_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Session" (
+    "id" TEXT NOT NULL,
+    "sessionToken" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "expires" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Session_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "VerificationToken" (
+    "identifier" TEXT NOT NULL,
+    "token" TEXT NOT NULL,
+    "expires" TIMESTAMP(3) NOT NULL
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_username_key" ON "User"("username");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Account_provider_providerAccountId_key" ON "Account"("provider", "providerAccountId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Session_sessionToken_key" ON "Session"("sessionToken");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "VerificationToken_token_key" ON "VerificationToken"("token");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "VerificationToken_identifier_token_key" ON "VerificationToken"("identifier", "token");
+
+-- AddForeignKey
+ALTER TABLE "Activity" ADD CONSTRAINT "Activity_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Account" ADD CONSTRAINT "Account_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Session" ADD CONSTRAINT "Session_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
